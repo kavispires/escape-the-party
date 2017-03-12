ed6123efa492ed5951c86b42fb0603e2bcb41522
kavispires <kavispires@gmail.com>
2017-03-11 23:54:43 -0800

    Roll/Save/Return actions complete

--- PLAN.md
+++ PLAN.md
@@ -3,5 +3,62 @@
 - Place Starting room
 - Place Player
 - Place 5 dice on Boot face in the 'tray area'.
-- 
+- Shuffle the 10 rooms in the roomsArray
+- Add the exit to roomsArray
 
+## Rooms
+- Starting: 2 horizontal doors
+- 2LR: 2 doors, left-right
+- 2LT: 2 doors, left-top
+- 1B: 1 door, bottom
+- 3LTR: 3 doors, left-top-right
+
+
+## Levels
+
+Level 1:
+- 5 rooms (EL, S, 2LR, 2LT, 1B*)
+- 1 friend
+
+Level 2: 
+- 6 rooms (EL, S, 3LTR, 1B*, 2LB, 1T*)
+- 2 friends
+
+Level 3: 
+- 7 rooms (EB, 2TR, S, 3LTR, 1T, 2)
+- 2 friends
+
+# TO-DO
+ROLL
+- Roll voice commands [OK]
+- Dice roll [OK]
+- TT dice are locked [OK]
+- Dice can be saved [OK]
+- Dice can be unsaved [OK]
+- If no slots available to lock TT, place regular saved die back to tray. [OK]
+- If 5 TT dice are locked, place the player back to the starting room.
+MAP
+- Create 4 level-1 maps
+ACTIONS
+- Move
+    + check if direction is allowed;
+    + check if dice is available in tray or safeSlots; 
+    + move character;
+    + update currentRoom;
+    + move used dice to tray and chance face to 'reroll';
+    + play steps sound;
+- Find Room
+    + check if door is allowed
+    + check if dice is available in tray or safeSlots;
+    + show room (attach it to map);
+    + move used dice to tray and chance face to 'reroll';
+    + play door sound;
+- Say Bye
+    + check if bye is allowed;
+    + check if dice is available in tray or safeSlots;
+    + show 'bye' balloon speech;
+    + move used dice to tray and chance face to 'reroll';
+    + play bye sound;
+- Show Exit;
+    + check if all friends were said bye
+    + show/attach exit
--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
 'use strict';
 
 const utils = {
-	randomDiceFace() {
-		return this.randomNumber(5,1);
+	randomDieFace() {
+		let rand = this.randomNumber(6,1);
+		if (rand === 6) rand = 1; // since there are 2 shoes, if second is chosen, reasign to 1
+		return rand;
 	},
 	randomNumber(max, min = 0) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	},
-	findAvailableSlot(array, el = 1) {
+	findAvailableSlot(array, el = 'x') {
 		for (let i = 0; i < array.length; i++) {
 			if (!array[i]) {
 				array[i] = el;
 				return i;
 			}
 		}
-		window.alert('No available slot.');
-		return 'No Available Slots.';
+		console.log('No available slot.');
+		return null;
+	},
+	findRegularDieSlot(array) {
+		for (let i = 0; i < array.length; i++) {
+			if (array[i].frame !== 5) return i;
+		}
+		return null;
+	},
+	print(message) {
+		console.log(message);
+		$('#log').prepend(`<li>${message}</li>`);
 	}
 };
 
@@ -51,16 +63,18 @@ var GameInstance = function() {
 	this.player = null;
 
 	this.dice = null;
-	this.safeSlots = [null, null, null, null];
-	this.safeSlotsPos = [19, 87, 155, 222]; // x positions
-	this.firstRoll = false;
+	this.tray = [];
+	this.safeSlots = [];
 
 	// This object holds all states for voice commands triggered by annyang
 	this.voiceCommands = {
 		up: false,
 		right: false,
 		down: false,
-		left: false
+		left: false,
+		save: false,
+		roll: false,
+		unsave: false
 	};
 };
 
@@ -93,10 +107,10 @@ GameInstance.prototype.create = function() {
 	// Add player sprite
 	this.player = this.game.add.sprite(359.5, 224.5, 'player');
 	// Add 5 dice
-	this.dice = this.game.add.group();
 	for (let i = 0; i < 5; i++) {
-		const die = this.dice.create(460 + i * 67, 523, 'die');
+		const die = this.game.add.sprite(460 + i * 67, 523, 'die');
 		die.safe = false;
+		this.tray.push(die);
 	}
 	// Create header information (level, friends number)
 	this.header.level = this.game.add.text(16, 8, 'Level ' + this.currentLevel.level, { fontSize: '32px', fill: '#000'});
@@ -107,78 +121,158 @@ GameInstance.prototype.create = function() {
 	/*this.timer.timer = this.game.timer.create(false);
 	this.timer.timer.loop(1000, this.timer.update, this);
 	this.timer.timer.start();*/
-	console.log(this.dice);
 };
 
 GameInstance.prototype.update = function() {
+	// Reposition every dice
+	this.tray.forEach((die, i) => {
+		die.x = 460 + i * 67;
+	});
+	this.safeSlots.forEach((die, i) => {
+		die.x = 19 + i * 67;
+	});
+
 	if (this.pause === true) return;
+
 	// Handles player movement
 	if (this.voiceCommands.up) {
 		this.player.y -= 90;
 		this.voiceCommands.up = false;
 	}
-	if (this.voiceCommands.down) {
+	else if (this.voiceCommands.down) {
 		this.player.y += 90;
 		this.voiceCommands.down = false;
 	}
-	if (this.voiceCommands.left) {
+	else if (this.voiceCommands.left) {
 		this.player.x -= 90;
 		this.voiceCommands.left = false;
 	}
-	if (this.voiceCommands.right) {
+	else if (this.voiceCommands.right) {
 		this.player.x += 90;
 		this.voiceCommands.right = false;
 	}
-	// Handles the timer
-	if (this.timer.on) {
-		
-	}
-	// Dice roll
+
+	// Handls dice roll
 	if (this.voiceCommands.roll) {
 		this.voiceCommands.roll = false;
 		// Randomize all non-saved dice faces
-		this.dice.children.forEach((die) => {
-			if (!die.safe)  {
-				die.frame = utils.randomDiceFace();
-				// If new die face is a 'shy', lock it.
-				if (die.frame === 3) { 
-					// Check next available slot on safeSlots array, move die to that slot, chante safe state to true
-					const slot = utils.findAvailableSlot(this.safeSlots);
-					console.log('SLOT', slot);
-					if (!isNaN(slot)) {
-						die.x = this.safeSlotsPos[slot];
-						die.safe = true;
+		this.tray.forEach((die, index) => {
+			die.frame = utils.randomDieFace();
+			console.log(die.frame);
+		});
+		// Check for TT/Shy
+		for (let d = 0; d < this.tray.length; d++) {
+			if (this.tray[d].frame === 5) {
+				utils.print('Got T_T');
+				const currentDie = this.tray.splice(d, 1);
+				d--;
+				// If there are still available slots on safeSlots array, add it
+				if (this.safeSlots.length >= 4) {
+					// If there are no available safeSlots, look for any regular dice
+					let regularDie;
+					for (let i = 0; i < this.safeSlots.length; i++) {
+						if (this.safeSlots[i].frame !== 5) {
+							regularDie = this.safeSlots.splice(i, 1);
+							this.tray.push(regularDie[0]);
+							break;
+						}
 					}
+					if (!regularDie) window.alert('GAME OVER'); //TO-DO: Return player to starting room
+				}
+				this.safeSlots.unshift(currentDie[0]);
+			}
+		}
+
+		// If any tray die face is a crown/courage, unlock a sy/TT in safeSlots if possible
+		for (let d = 0; d < this.tray.length; d++) {
+			if (this.tray[d].frame === 4) {
+				for (let i = 0; i < this.safeSlots.length; i++) {
+					if (this.safeSlots[i].frame === 5) {
+						const dieToReturn = this.safeSlots.shift();
+						dieToReturn.frame = 0;
+						this.tray[d].frame = 0;
+						this.tray.push(dieToReturn);
+						utils.print('Unlocked TT with Courage/Crown.');
+						break;
+					} 
 				}
 			}
-		});
+		}
 
+		console.log('TRAY', this.tray);
+		console.log('SAFE', this.safeSlots);
+	}
+
+	// Handles dice lock/save
+	if (this.voiceCommands.save) {
+		console.log('SAFE', this.voiceCommands.save);
+		// Check if die is available to be saved
+		let found;
+		for (let i = 0; i < this.tray.length; i++) {
+			const die = this.tray[i];
+			if (die.frame === this.voiceCommands.save && this.safeSlots.length < 4) {
+				const currentDie = this.tray.splice(i, 1);
+				this.safeSlots.push(currentDie[0]);
+				found = true;
+				break;
+			}
+		}
+		if (!found) utils.print('Die not available for saving.');
+		this.voiceCommands.save = false;
+	}
+
+	// Handles dice return to tray
+	if (this.voiceCommands.unsave) {
+		console.log('RETURN', this.voiceCommands.unsave);
+		// Check if die is available to be returned
+		let found;
+		for (let i = 0; i < this.safeSlots.length; i++) {
+			const die = this.safeSlots[i];
+			if (die.frame === this.voiceCommands.unsave) {
+				const currentDie = this.safeSlots.splice(i, 1);
+				this.tray.push(currentDie[0]);
+				found = true;
+				break;
+			}
+		}
+		if (!found) utils.print('Die not available for returning');
+		this.voiceCommands.unsave = false;
 	}
 };
-
-
 
 // Start annyang voice commands
 if (annyang) {
 
 	var commands = {
 		'hello': function() {
-			console.log('Hi!');
+			utils.print('Hi!');
 		},
 		'pause': function() {
 			newGame.pause = !newGame.pause;
-			console.log('Pause', newGame.pause);
+			utils.print('Pause', newGame.pause);
 		},
 		'move :direction': function(direction) {
-			console.log('Moving:', direction);
+			utils.print('Moving: ' + direction);
 			if (direction === 'up' || direction === 'north') newGame.voiceCommands.up = true;
 			else if (direction === 'down' || direction === 'South') newGame.voiceCommands.down = true;
 			else if (direction === 'left' || direction === 'west') newGame.voiceCommands.left = true;
 			else if (direction === 'right' || direction === 'East') newGame.voiceCommands.right = true;
 		},
 		'roll': function() {
-			console.log('Rolling');
+			utils.print('Rolling');
 			newGame.voiceCommands.roll = true;
+		},
+		'save :face': function(face) {
+			utils.print('Saving: ' + face);
+			if (face === 'boot' || face === 'shoe' || face === 'foot' || face === 'shoes' || face === 'shoot') newGame.voiceCommands.save = 1;
+			if (face === 'hand' || face === 'glove' || face === 'him') newGame.voiceCommands.save = 2;
+			if (face === 'door') newGame.voiceCommands.save = 3;
+		},
+		'return :face': function(face) {
+			utils.print('Returning: ' + face);
+			if (face === 'boot' || face === 'shoe' || face === 'foot' || face === 'shoes' || face === 'shoot') newGame.voiceCommands.unsave = 1;
+			if (face === 'hand' || face === 'glove' || face === 'him') newGame.voiceCommands.unsave = 2;
+			if (face === 'door') newGame.voiceCommands.unsave = 3;
 		}
 	};
 
